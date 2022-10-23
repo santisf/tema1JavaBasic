1de39885f059ceb3108a3ded632ccf6e4119a8df
Santiago Foti <santiago.foti1@gmail.com>
2022-10-22 22:10:18 -0300

    Tema 1 tarea

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,35 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+
+        System.out.println("Examen");
+        System.out.println("Tipos de datos más comúnes: ");
+        System.out.println("Primitivos: ");
+        int numeroEnteroInt=45;
+        System.out.println("Numeros enteros int: "+ numeroEnteroInt);
+        long numeroEnteroLong=456153964546456l;
+        System.out.println("Numeros enteros long: "+ numeroEnteroLong);
+        float numeroDecimalFloat=3.9f;
+        System.out.println("Numero decimal float: "+ numeroDecimalFloat);
+        double numeroDecimalDouble=3.9456d;
+        System.out.println("Numero decimal double: "+ numeroDecimalDouble);
+        char unCaracter='c';
+        System.out.println("Caracter: "+ unCaracter);
+        boolean booleanVerdadero=true;
+        System.out.println("Booleano Verdadero: "+ booleanVerdadero);
+        boolean booleanFalso=false;
+        System.out.println("Booleano Falso: "+ booleanFalso);
+        System.out.println("Envolventes: ");
+        Integer numeroEnteroEnvolventeInterger=345;
+        System.out.println("Numeros enteros Integer: "+ numeroEnteroEnvolventeInterger);
+        Long numeroEnteroEnvolventeLong= 3435356754L;
+        System.out.println("Numeros enteros Long: "+ numeroEnteroEnvolventeLong);
+        String cadenaDeTexto= "Santiago";
+        System.out.println("Cadenas de texto String: "+ cadenaDeTexto);
+
+
+
+
+
+
     }
 }
